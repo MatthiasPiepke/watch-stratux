7cc13f5fc8afd4e6f2831fa9ad38f296414c602d
Matthias Piepke <matdevapps@gmx.de>
2023-08-06 17:04:03 +0200

    version 0.4.8 - some clean ups in StratuxWsReceiver.kt - some clean ups in main_recycler_item_view_2.xml

--- app/src/main/java/com/example/watchstratux/StratuxWsReceiver.kt
+++ app/src/main/java/com/example/watchstratux/StratuxWsReceiver.kt
@@ -17,23 +17,20 @@ class StratuxBooleanData(var key: String, var value: Boolean)
 class StratuxIntData(var key: String, var value: Int)
 
 object StratuxStatusData {
-    var version: StratuxStringData = StratuxStringData("Version", "")                           // string
-    var cpuTemp: StratuxDoubleData = StratuxDoubleData("CPUTemp", 0.0)                           // double
-    var gpsConnected: StratuxBooleanData = StratuxBooleanData("GPS_connected", false)                // bool
-    var gpsSolution: StratuxStringData = StratuxStringData("GPS_solution", "")                  // string
-    var gpsSatelliteTracked: StratuxIntData = StratuxIntData("GPS_satellites_locked", 0) // int
-    var gpsSatelliteSeen: StratuxIntData = StratuxIntData("GPS_satellites_seen", 0) // int
-    var bmpConnected: StratuxBooleanData = StratuxBooleanData("BMPConnected", false)                 // bool
-    var imuConnected: StratuxBooleanData = StratuxBooleanData("IMUConnected", false)                 // bool
-    var ognConnected: StratuxBooleanData = StratuxBooleanData("OGN_connected", false)                // bool
-    var ognNoise: StratuxDoubleData = StratuxDoubleData("OGN_noise_db", 0.0)                     // double
-    var ognGain: StratuxDoubleData = StratuxDoubleData("OGN_gain_db", 0.0)                       // double
-    var esTrafficTracking: StratuxIntData = StratuxIntData("ES_traffic_targets_tracking", 0) // int
-    var esMessagesLastMinute: StratuxIntData = StratuxIntData("ES_messages_last_minute", 0)  // int
-    var esMessagesMax: StratuxIntData = StratuxIntData("ES_messages_max", 0)  // int
-    var ognMessagesLastMinute: StratuxIntData = StratuxIntData("OGN_messages_last_minute", 0)  // int
-    var ognMessagesMax: StratuxIntData = StratuxIntData("OGN_messages_max", 0)  // int
-    var sdrDevices: StratuxIntData = StratuxIntData("Devices", 0)  // int
+    var version: StratuxStringData = StratuxStringData("Version", "")
+    var cpuTemp: StratuxDoubleData = StratuxDoubleData("CPUTemp", 0.0)
+    var gpsConnected: StratuxBooleanData = StratuxBooleanData("GPS_connected", false)
+    var gpsSolution: StratuxStringData = StratuxStringData("GPS_solution", "")
+    var gpsSatelliteTracked: StratuxIntData = StratuxIntData("GPS_satellites_locked", 0)
+    var gpsSatelliteSeen: StratuxIntData = StratuxIntData("GPS_satellites_seen", 0)
+    var ognConnected: StratuxBooleanData = StratuxBooleanData("OGN_connected", false)
+    var ognNoise: StratuxDoubleData = StratuxDoubleData("OGN_noise_db", 0.0)
+    var ognGain: StratuxDoubleData = StratuxDoubleData("OGN_gain_db", 0.0)
+    var ognMessagesLastMinute: StratuxIntData = StratuxIntData("OGN_messages_last_minute", 0)
+    var ognMessagesMax: StratuxIntData = StratuxIntData("OGN_messages_max", 0)
+    var esTrafficTracking: StratuxIntData = StratuxIntData("ES_traffic_targets_tracking", 0)
+    var esMessagesLastMinute: StratuxIntData = StratuxIntData("ES_messages_last_minute", 0)
+    var esMessagesMax: StratuxIntData = StratuxIntData("ES_messages_max", 0)
     var upTimeClock: StratuxStringData = StratuxStringData("UptimeClock", "")
     var newData = false
 }
@@ -58,17 +55,14 @@ class StratuxWsReceiver() {
             StratuxStatusData.gpsSolution.value = jsonconversion.getString(StratuxStatusData.gpsSolution.key)
             StratuxStatusData.gpsSatelliteTracked.value = jsonconversion.getInt(StratuxStatusData.gpsSatelliteTracked.key)
             StratuxStatusData.gpsSatelliteSeen.value = jsonconversion.getInt(StratuxStatusData.gpsSatelliteSeen.key)
-            //StratuxStatusData.bmpConnected.value = jsonconversion.getBoolean(StratuxStatusData.bmpConnected.key)
-            //StratuxStatusData.imuConnected.value = jsonconversion.getBoolean(StratuxStatusData.imuConnected.key)
             StratuxStatusData.ognConnected.value = jsonconversion.getBoolean(StratuxStatusData.ognConnected.key)
             StratuxStatusData.ognNoise.value = jsonconversion.getDouble(StratuxStatusData.ognNoise.key)
             StratuxStatusData.ognGain.value = jsonconversion.getDouble(StratuxStatusData.ognGain.key)
+            StratuxStatusData.ognMessagesLastMinute.value = jsonconversion.getInt(StratuxStatusData.ognMessagesLastMinute.key)
+            StratuxStatusData.ognMessagesMax.value = jsonconversion.getInt(StratuxStatusData.ognMessagesMax.key)
             StratuxStatusData.esTrafficTracking.value = jsonconversion.getInt(StratuxStatusData.esTrafficTracking.key)
             StratuxStatusData.esMessagesLastMinute.value = jsonconversion.getInt(StratuxStatusData.esMessagesLastMinute.key)
             StratuxStatusData.esMessagesMax.value = jsonconversion.getInt(StratuxStatusData.esMessagesMax.key)
-            StratuxStatusData.ognMessagesLastMinute.value = jsonconversion.getInt(StratuxStatusData.ognMessagesLastMinute.key)
-            StratuxStatusData.ognMessagesMax.value = jsonconversion.getInt(StratuxStatusData.ognMessagesMax.key)
-            StratuxStatusData.sdrDevices.value = jsonconversion.getInt(StratuxStatusData.sdrDevices.key)
             StratuxStatusData.upTimeClock.value = jsonconversion.getString(StratuxStatusData.upTimeClock.key)
             if(BuildConfig.DEBUG) Log.i(TAG, "Message")
             StratuxStatusData.newData = true
